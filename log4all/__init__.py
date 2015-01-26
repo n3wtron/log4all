73dadcf3790c7d583ad5df5723e72e39b98427bf
Igor Maculan <igor.maculan@infocamere.it>
2015-01-26 19:50:33 +0100

    divided initialization functions

--- log4all/__init__.py
+++ log4all/__init__.py
@@ -15,10 +15,10 @@ applications = None
 dt_applications_updated = None
 
 
-def initdb(db):
+def init_db(db):
     """
     Database Initialization
-    :param db:
+    :param db:mongodb connection
     """
     Log.init(db)
     Stack.init(db)
@@ -26,16 +26,38 @@ def initdb(db):
     Application.init(db)
 
 
-def main(global_config, **settings):
-    config = Configurator(settings=settings)
-    config.include('pyramid_jinja2')
-    config.add_renderer('.jsinja2', 'pyramid_jinja2.renderer_factory')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_static_view('admin_static', 'admin_static', cache_max_age=3600)
-    config.add_static_view('bower_components', 'bower_components', cache_max_age=3600)
+def init_routing(config):
+    """
+    Routing initialization
+    :param config: pyramid configuration
+    """
 
-    # MongoDB
-    db_url = urlparse(settings['mongo_uri'])
+    config.add_route('home', '/')
+    config.add_route('api_logs_add', '/api/logs/add')
+    config.add_route('api_logs_search', '/api/logs/search')
+    config.add_route('api_logs_tail', '/api/logs/tail')
+    config.add_route('helper_applications_autocomplete', 'api/applications/autocompleteSearch')
+    config.add_route('api_applications_add', '/api/applications/add')
+    config.add_route('api_applications_all', '/api/applications')
+
+    config.add_route('api_application_get', '/api/application/get')
+    config.add_route('api_application_delete', '/api/application/delete')
+    config.add_route('api_application_update', '/api/application/update')
+    config.add_route('api_tags_get', '/api/tags')
+    config.add_route('api_stack_get', '/api/stack')
+
+    config.add_route('admin', '/admin')
+    config.add_route('admin_js', '/admin/js/admin_log4all.js')
+    config.add_route('admin_applications', '/admin/applications')
+    config.add_route('admin_application_edit', '/admin/application/edit')
+
+
+def init_log4all(config):
+    """
+    Log4all initialization, request_method and db initialization
+    :param config: pyramid configuration
+    """
+    db_url = urlparse(config.get_settings()['mongo_uri'])
     config.registry.db = pymongo.Connection(host=db_url.hostname, port=db_url.port)
 
     def get_db(request):
@@ -60,31 +82,24 @@ def main(global_config, **settings):
                 applications[app.name] = app
         return applications.get(application_name)
 
-    initdb(get_db(None))
+    init_db(get_db(None))
 
     config.add_request_method(get_db, 'db', reify=True)
     config.add_request_method(get_gridfs, 'gridfs', reify=True)
     config.add_request_method(get_application, 'applications')
 
-    # Routing
-    config.add_route('home', '/')
-    config.add_route('api_logs_add', '/api/logs/add')
-    config.add_route('api_logs_search', '/api/logs/search')
-    config.add_route('api_logs_tail', '/api/logs/tail')
-    config.add_route('helper_applications_autocomplete', 'api/applications/autocompleteSearch')
-    config.add_route('api_applications_add', '/api/applications/add')
-    config.add_route('api_applications_all', '/api/applications')
 
-    config.add_route('api_application_get', '/api/application/get')
-    config.add_route('api_application_delete', '/api/application/delete')
-    config.add_route('api_application_update', '/api/application/update')
-    config.add_route('api_tags_get', '/api/tags')
-    config.add_route('api_stack_get', '/api/stack')
+def main(global_config, **settings):
+    config = Configurator(settings=settings)
+    config.include('pyramid_jinja2')
+    config.add_renderer('.jsinja2', 'pyramid_jinja2.renderer_factory')
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('admin_static', 'admin_static', cache_max_age=3600)
+    config.add_static_view('bower_components', 'bower_components', cache_max_age=3600)
 
-    config.add_route('admin', '/admin')
-    config.add_route('admin_js', '/admin/js/admin_log4all.js')
-    config.add_route('admin_applications', '/admin/applications')
-    config.add_route('admin_application_edit', '/admin/application/edit')
+    init_log4all(config)
+
+    init_routing(config)
 
     config.scan()
     return config.make_wsgi_app()
